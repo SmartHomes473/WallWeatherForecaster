dfc8034a303e7039095cc7c27aa1d4796e8e4308
Chrs Fulara <fularac@0587351918.wireless.umich.net>
2014-12-03 18:28:52 -0500

    Forgot the header too

---
+++ libraries/WWF/Display.h
@@ -0,0 +1,11 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+class Display{
+ public:
+  void dumbDisplay();
+  String displayTemp(int temp_index, String temperature);
+  int digitCounter(String temp);
+};
+
+#endif
